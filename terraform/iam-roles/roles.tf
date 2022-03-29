2bb4041449cb05839b769420aedf8b5326fdc3d3
mjki4hj3 <hudeyfa.jama@gmail.com>
2022-03-29 08:03:21 +0100

    added role and policy for ecs_tasks, codepipeline, codebuild

--- terraform/iam-roles/roles.tf
+++ terraform/iam-roles/roles.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 #
-resource "aws_iam_role_policy" "ecs_service_policy" {
+resource "aws_iam_role_policy" "ecs_service_role_policy" {
   name = "ecs_policy"
   role = aws_iam_role.ecs_service_role.id
 
@@ -65,6 +65,207 @@ resource "aws_iam_role" "ecs_service_role" {
           Service = [
             "ec2.amazonaws.com",
             "ecs-tasks.amazonaws.com"
+          ]
+        }
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy" "ecs_task_role_policy" {
+  name = "ecs_task"
+  role = aws_iam_role.ecs_task_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "logs:CreateLogStream",
+          "logs:CreateLogGroup",
+          "logs:PutLogEvents"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "dynamodb:Scan",
+          "dynamodb:Query",
+          "dynamodb:UpdateItem",
+          "dynamodb:GetItem"
+        ]
+        Effect = "Allow"
+        Resource = "arn:aws:dynamodb:*:*:table/GetScheduledTable*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs_task_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = [
+            "ecs-tasks.amazonaws.com"
+          ]
+        }
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy" "getscheduled_codepipeline_service_role_policy" {
+  name = "getscheduled_codepipeline_service_policy"
+  role = aws_iam_role.getscheduled_codebuild_service_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "codecommit:GetBranch",
+          "codecommit:GetCommit",
+          "codecommit:UploadArchive",
+          "codecommit:GetUploadArchiveStatus",
+          "codecommit:CancelUploadArchive"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+           "s3:GetObjectVersion",
+           "s3:GetBucketVersioning",
+           "s3:GetObject"
+
+        ]
+        Effect = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "s3:PutObject"
+        ]
+        Effect = "Allow"
+        Resource = "arn:aws:s3:::*"
+      },
+      {
+        Action = [
+          "elasticloadbalancing:*",
+          "autoscaling:*",
+          "cloudwatch:*",
+          "ecs:*",
+          "codebuild:*",
+          "iam:PassRole"
+        ]
+        Effect = "Allow"
+        Resource = "*"
+      },
+      
+    ]
+  })
+}
+
+resource "aws_iam_role" "getscheduled_codepipeline_service_role" {
+  name = "getscheduled_codepipeline_service_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = [
+            "codepipeline.amazonaws.com"
+          ]
+        }
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy" "getscheduled_codebuild_service_role_policy" {
+  name = "getscheduled_codebuild_service_policy"
+  role = aws_iam_role.getscheduled_codebuild_service_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "codecommit:ListBranches",
+          "codecommit:ListRepositories",
+          "codecommit:BatchGetRepositories",
+          "codecommit:Get*",
+          "codecommit:GitPull"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+
+        ]
+        Effect = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:ListBucket"
+
+        ]
+        Effect = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "ecr:InitiateLayerUpload",
+          "ecr:GetAuthorizationToken"
+        ]
+        Effect = "Allow"
+        Resource = "*"
+      },
+      
+    ]
+  })
+}
+
+resource "aws_iam_role" "getscheduled_codebuild_service_role" {
+  name = "getscheduled_codebuild_service_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = [
+            "codebuild.amazonaws.com"
           ]
         }
       },
