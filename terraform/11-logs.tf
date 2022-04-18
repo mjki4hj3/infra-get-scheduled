fcaa6b566200d8273feb2cf835c606478e92d4f0
mjki4hj3 <hudeyfa.jama@gmail.com>
2022-04-18 12:15:55 +0100

    changed port, added link to docs

--- terraform/11-logs.tf
+++ terraform/11-logs.tf
@@ -1,3 +1,4 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_group
 resource "aws_cloudwatch_log_group" "getscheduled-logs" {
   name = "getscheduled-logs"
 }
